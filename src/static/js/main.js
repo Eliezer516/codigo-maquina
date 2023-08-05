d7375788599a080761498827dad657387d3d3c66
Eliezer516 <ybarretomaterano@gmail.com>
2023-08-05 16:33:02 -0400

    add functional search bar and some styles

--- src/static/js/main.js
+++
@@ -1,5 +0,0 @@
-;(() => {
-  fetch('http://localhost:8080/static/post.json')
-    .then(res => res.json())
-    .then(data => console.log(data))
-})()
--- src/static/js/search.js
+++ src/static/js/search.js
@@ -1,6 +1,47 @@
-const form = document.querySelector("#searchForm")
+if (location.pathname === '/search/') {
+  const entrada = document.querySelector("#searchbox")
+  const wrapperResults = document.querySelector("#wrapper-results")
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault()
-  alert("hola")
-})
+  const getPosts = async () => {
+    const response = await fetch('/static/post.json')
+    const data = await response.json()
+    return data
+  }
+
+  entrada.addEventListener("keyup", async (e) => {
+    
+    const posts = await getPosts()
+    const results = fuzzysort.go(e.target.value, posts, { key: 'title' })
+
+    wrapperResults.innerHTML = ''
+
+    results.forEach(item => {
+      const { title, date, url, imgUrl } = item.obj
+
+      const card = templateCard(title, date, url, imgUrl)
+      wrapperResults.innerHTML += card
+    })
+
+  })
+
+  const templateCard = (title, date, url, imgUrl) => {
+    return `
+      <div class="rounded overflow-hidden shadow">
+        <div class="flex flex-col">
+          <div class="w-full px-6 py-4">
+            <img class="rounded-lg" src="${imgUrl}" alt="Imagen de la noticia" width="300" height="200">
+          </div>
+          <div class="w-full px-6 py-4">
+            <div class="font-bold text-xl mb-2"><a class="cursor-pointer" data-barba-prevent="self" href="${url}">${title}</a></div>
+            <p class="text-gray-700 text-base"></p>
+            <div class="mt-8 flex">
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Etiqueta</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">Etiqueta</span>
+              <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">Etiqueta</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    `
+  }
+}
